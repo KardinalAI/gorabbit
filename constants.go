6629dd7507a10fdec26e23367a7c02ba3b4aff88
Alex El Khoury <alexandre.elkhoury@kardinal.ai>
2021-05-24 11:28:58 +0200

    remove constants

--- constants.go
+++ constants.go
@@ -8,31 +8,8 @@ const (
 )
 
 const (
-	SolangeEventQueue   = "solange_event_queue"
-	SolangeCommandQueue = "solange_command_queue"
-
-	ActivityEventQueue   = "activity_event_queue"
-	ActivityCommandQueue = "activity_command_queue"
-
-	PlanEventQueue   = "plan_event_queue"
-	PlanCommandQueue = "plan_command_queue"
-)
-
-const (
 	EventsExchange   = "events_exchange"
 	CommandsExchange = "commands_exchange"
-)
-
-const (
-	ConsumerSolange  = "solange_consumer"
-	ConsumerPlan     = "plan_consumer"
-	ConsumerActivity = "activity_consumer"
-)
-
-const (
-	MicroserviceSolange  = "solange"
-	MicroservicePlan     = "plan"
-	MicroserviceActivity = "activity"
 )
 
 const (
