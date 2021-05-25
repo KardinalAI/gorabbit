e9ebdfc2d325c9a38d0dcace683d7bd17a385f55
Alex El Khoury <alexandre.elkhoury@kardinal.ai>
2021-05-25 15:56:33 +0200

    delete example

--- example/go.mod
+++
@@ -1,5 +0,0 @@
-module gitlab.kardinal.ai/coretech/gorabbit/example
-
-go 1.16
-
-require gitlab.kardinal.ai/coretech/gorabbit v0.1.2
--- example/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xterm -title "Setup" -e "go run setup_from_yml.go" &&
-  xterm -title "Receiver" -e 'go run receive.go' &
-xterm -title "Sender" -e "go run send.go" &
